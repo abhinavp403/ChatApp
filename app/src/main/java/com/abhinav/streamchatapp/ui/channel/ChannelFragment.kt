739d8b5a0d0f13ea4c2954d0ae6c8e037f8ccfc2
abhinavp403 <abhinavp403@gmail.com>
2023-10-21 18:13:24 -0400

    Create channels within app

--- app/src/main/java/com/abhinav/streamchatapp/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/abhinav/streamchatapp/ui/channel/ChannelFragment.kt
@@ -3,18 +3,23 @@ package com.abhinav.streamchatapp.ui.channel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
+import com.abhinav.streamchatapp.R
 import com.abhinav.streamchatapp.databinding.FragmentChannelBinding
 import com.abhinav.streamchatapp.ui.BindingFragment
+import com.abhinav.streamchatapp.util.navigateSafely
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.ui.channel.list.header.viewmodel.ChannelListHeaderViewModel
 import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
 import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
 import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
 import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
+import kotlinx.coroutines.flow.collect
 
 class ChannelFragment : BindingFragment<FragmentChannelBinding>() {
 
@@ -25,7 +30,6 @@ class ChannelFragment : BindingFragment<FragmentChannelBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val user = viewModel.getUser()
         if(user == null) {
             findNavController().popBackStack()
@@ -37,14 +41,40 @@ class ChannelFragment : BindingFragment<FragmentChannelBinding>() {
             sort = ChannelListViewModel.DEFAULT_SORT,
             limit = 30
         )
-        val chanelViewModel: ChannelListViewModel by viewModels { factory }
-        val chanelHeaderViewModel: ChannelListHeaderViewModel by viewModels()
-        chanelViewModel.bindView(binding.channelListView, viewLifecycleOwner)
-        chanelHeaderViewModel.bindView(binding.channelListHeaderView, viewLifecycleOwner)
+        val channelListViewModel: ChannelListViewModel by viewModels { factory }
+        val channelListHeaderViewModel: ChannelListHeaderViewModel by viewModels()
+        channelListViewModel.bindView(binding.channelListView, viewLifecycleOwner)
+        channelListHeaderViewModel.bindView(binding.channelListHeaderView, viewLifecycleOwner)
 
         binding.channelListHeaderView.setOnUserAvatarClickListener {
             viewModel.logout()
             findNavController().popBackStack()
+        }
+
+        binding.channelListHeaderView.setOnActionButtonClickListener {
+            findNavController().navigateSafely(
+                R.id.action_channelFragment_to_createChannelDialog
+            )
+        }
+
+        binding.channelListView.setChannelItemClickListener { channel ->
+            findNavController().navigateSafely(
+                R.id.action_channelFragment_to_chatFragment,
+                Bundle().apply { putString("channelId", channel.cid) }
+            )
+        }
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.createChannelEvent.collect { event->
+                when(event) {
+                    is ChannelViewModel.CreateChannelEvent.Error -> {
+                        Toast.makeText(requireContext(), event.error, Toast.LENGTH_LONG).show()
+                    }
+                    is ChannelViewModel.CreateChannelEvent.Success -> {
+                        Toast.makeText(requireContext(), R.string.channel_created, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
         }
     }
 }
